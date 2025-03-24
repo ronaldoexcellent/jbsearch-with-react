49f559f574f7fef062a0ceaa2b0c84df64ff6f68
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-03-24 18:41:47 +0100

    Delete src directory

--- src/guests/components/LogOut.jsx
+++
@@ -1,9 +0,0 @@
-function LogOut({ handle }) {
-    return (
-        <>
-            <i className={`fas fa-sign-out animate-bounce cursor-pointer ${handle? "mr-3":""}`}></i>
-            { handle && (<span>Log Out</span>) }
-        </>
-    )
-}
-export default LogOut;
