fbf06037eebb993d0a3a122eea4842b95762c8b1
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-03-29 11:35:19 +0100

    Delete src directory

--- src/guests/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div className={`text-gray-800 bg-white md:mt-4 p-4 shadow flex justify-between items-center`}>
-            <h1 className="text-xl font-bold"> JBSearch  Copyright &copy; {new Date().getFullYear()}</h1>
-        </div>
-    );
-}
-
-export default Footer;
