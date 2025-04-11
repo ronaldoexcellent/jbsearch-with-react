c3f052713f445a699af1b93bff79d647854bb849
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-04-11 18:04:27 +0100

    Delete src directory

--- src/guests/components/loaders/spinnerloader/SpinnerLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { Oval } from 'react-loader-spinner';
-
-const SpinnerLoader = () => (
-  <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-    <Oval height={80} width={80} secondaryColor='#FFFFFFFF' strokeWidth="6" color="#010F24FF" ariaLabel="loading" />
-  </div>
-);
-
-export default SpinnerLoader;
