2b5c2a459ce3cecb45812d357dc361b3c6187d4b
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-04-20 17:35:11 +0100

    Delete src directory

--- src/guests/employer/utils/SidebarData.jsx
+++
@@ -1,8 +0,0 @@
-const SidebarData = [
-    { name: "Dashboard", icon: "fa-tachometer-alt", address: "/employer/", title: "Dashboard" },
-    { name: "Jobs", icon: "fa-briefcase", address: "/employer/jobs", title: "Jobs" },
-    { name: "Job Postings", icon: "fa-file-alt", address: "/employer/formbuilder", title: "Post A Job" },
-    { name: "Applicants", icon: "fa-user-circle", address: "/employer/applicants", title: "Applicants" }
-];
-
-export default SidebarData;
