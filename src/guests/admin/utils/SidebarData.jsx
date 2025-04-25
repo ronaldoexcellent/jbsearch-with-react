ab30b3a7b855808efd60854780dd0fe4a19305e5
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-04-25 19:02:22 +0100

    Delete src directory

--- src/guests/admin/utils/SidebarData.jsx
+++
@@ -1,8 +0,0 @@
-const SidebarData = [
-    { name: "Dashboard", icon: "fa-tachometer-alt", address: "/admin/", title: "Dahboard" },
-    { name: "Jobs", icon: "fa-briefcase", address: "/admin/jobs", title: "Jobs" },
-    { name: "Profile", icon: "fa-user", address: "/admin/profile", title: "Profile" },
-    { name: "Notifications", icon: "fa-bell", address: "/admin/notifications", title: "Notifications" }
-];
-
-export default SidebarData;
--- src/guests/employer/utils/SidebarData.jsx
+++
@@ -1,8 +0,0 @@
-const SidebarData = [
-    { name: "Dashboard", icon: "fa-tachometer-alt", address: "/employer/", title: "Dashboard" },
-    { name: "Jobs", icon: "fa-briefcase", address: "/employer/jobs", title: "Jobs" },
-    { name: "Profile", icon: "fa-user", address: "/employer/profile", title: "Profile" },
-    { name: "Job Postings", icon: "fa-file-alt", address: "/employer/formbuilder", title: "Post A Job" }
-];
-
-export default SidebarData;
